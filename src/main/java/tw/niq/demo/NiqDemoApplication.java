2e311f24f43371f67568c07f60c4efb037490874
Nick Chen <nick.chen@cmp.com.tw>
2023-12-26 12:07:00 +0800

    Initial commit

---
+++ src/main/java/tw/niq/demo/NiqDemoApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiqDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/niq/demo/NiqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.niq.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
